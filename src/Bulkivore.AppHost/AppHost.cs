d6fab6a0609be1b078aab16e25b0f8254a4afda6
solus <quocanhthedev@gmail.com>
2026-08-31 21:49:18 +0700

    feat(aspire): Add publishing docker compose

--- src/Bulkivore.AppHost/AppHost.cs
+++ src/Bulkivore.AppHost/AppHost.cs
@@ -1,11 +1,29 @@
-var builder = DistributedApplication.CreateBuilder(args);
+#pragma warning disable ASPIRECOMPUTE003
+#pragma warning disable ASPIREPIPELINES003
 
-var postgres = builder.AddPostgres("postgres");
+var builder = DistributedApplication.CreateBuilder(args);
+var registry = builder.AddContainerRegistry("ghcr", "ghcr.io", "sirnacou/bulkivore");
+
+builder.AddDockerComposeEnvironment("env");
+
+var postgres = builder.AddPostgres("postgres")
+	.WithDataVolume()
+	.WithDbx();
 var db = postgres.AddDatabase("bulkivore-db");
+var testDb = postgres.AddDatabase("bulkivore-test-db");
 
 var api = builder.AddProject<Projects.Bulkivore_Api>("api")
+	.PublishAsDockerComposeService((resource, service) => { service.Name = "api"; })
+	.WithContainerRegistry(registry)
+	.WithRemoteImageName("api")
+	.WithRemoteImageTag("latest")
 	.WithReference(db)
+	.WithReference(testDb)
 	.WaitFor(db)
-	.WithHttpHealthCheck("/health");
+	.WithHttpHealthCheck("/health")
+	.WithEnvironment(ctx => { ctx.EnvironmentVariables["TEST_DB_CONN"] = testDb.Resource.UriExpression; });
 
 builder.Build().Run();
+
+#pragma warning restore ASPIREPIPELINES003
+#pragma warning restore ASPIRECOMPUTE003
